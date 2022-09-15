9a192635ec8b471b729c77f5fa607d037201884c
tomohiro-tanazawa <tanasrn23@gmail.com>
2022-09-15 18:32:40 +0900

    別サイトの方法を参考にやり直し

--- serverspec/spec_helper.rb
+++ serverspec/spec_helper.rb
@@ -1,20 +1,13 @@
 require 'serverspec'
-require 'pathname'
 require 'net/ssh'
 
-include SpecInfra::Helper::Ssh
-include SpecInfra::Helper::DetectOS
+include Serverspec::Helper::Ssh
+include Serverspec::Helper::DetectOS
 
 RSpec.configure do |c|
-  if ENV['ASK_SUDO_PASSWORD']
-    require 'highline/import'
-    c.sudo_password = ask("Enter sudo password: ") { |q| q.echo = false }
-  else
-    c.sudo_password = ENV['SUDO_PASSWORD']
-  end
   c.host  = ENV['TARGET_HOST']
   options = Net::SSH::Config.for(c.host)
   user    = options[:user] || Etc.getlogin
   c.ssh   = Net::SSH.start(c.host, user, options)
-  c.os    = backend.check_os
+  c.os    = backend(Serverspec::Commands::Base).check_os
 end
